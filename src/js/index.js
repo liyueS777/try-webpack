4247cd8d4dcd65dc62801fbed4d37c177a6f5764
myLiyue <steve_li@kingdee.com>
2018-09-19 20:13:23 +0800

    'dev02'

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import bar from './bar'
+import '../css/index.css'
+import icon from '../imgs/shalong.jpg'
+bar()
+var myIcon = new Image();
+myIcon.src=icon;
+document.body.appendChild(myIcon);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app:'./src/js/index.js',
+    print:'./src/js/print.js'
+  },
+  plugins:[
+    //先清除掉
+    // new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title:'Output Mangement',
+      template:'index1.html',
+      filename:path.resolve(__dirname,'./dist')
+    }),
+
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'//这里的[name]是根据入口entry来的
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test:/\.(png|svg|jpg|gif)$/,
+        use:['file-loader']
+      }
+    ]
   }
 };
